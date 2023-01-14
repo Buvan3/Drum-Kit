7c9927c78b89b8cf17769a4f6a25d435afd90289
Bu1 <115283509+Buvan3@users.noreply.github.com>
2023-01-14 16:24:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+
+
+document.querySelector(".w").addEventListener("click",handleClick1);
+
+function handleClick1() {
+
+    var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
+} 
+
+document.querySelector(".a").addEventListener("click",handleClick2);
+
+function handleClick2() {
+
+    var audio = new Audio("sounds/tom-2.mp3");
+    audio.play();
+} 
+
+document.querySelector(".s").addEventListener("click",handleClick3);
+
+function handleClick3() {
+
+    var audio = new Audio("sounds/tom-3.mp3");
+    audio.play();
+} 
+
+document.querySelector(".d").addEventListener("click",handleClick4);
+
+function handleClick4() {
+
+    var audio = new Audio("sounds/tom-4.mp3");
+    audio.play();
+} 
+
+document.querySelector(".j").addEventListener("click",handleClick5);
+
+function handleClick5() {
+
+    var audio = new Audio("sounds/crash.mp3");
+    audio.play();
+} 
+
+document.querySelector(".k").addEventListener("click",handleClick6);
+
+function handleClick6() {
+
+    var audio = new Audio("sounds/kick-bass.mp3");
+    audio.play();
+} 
+
+document.querySelector(".l").addEventListener("click",handleClick7);
+
+function handleClick7() {
+
+    var audio = new Audio("sounds/snare.mp3");
+    audio.play();
+} 
+
